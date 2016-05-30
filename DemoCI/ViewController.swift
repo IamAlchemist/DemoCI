6663ed07dfe9645f3cbfb5f0839920b80598e1c1
Wizard Lee <282539259@qq.com>
2016-05-30 15:05:58 +0800

    init commit

---
+++ DemoCI/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  DemoCI
+//
+//  Created by Wizard Li on 5/30/16.
+//  Copyright © 2016 cc.kauhaus. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
